44c219aad9514ac71eb5b3fae69b64e7ad854a43
joeun-01 <jjy020121@gmail.com>
2023-01-20 05:20:31 +0900

    fix : solve conflict

--- src/main/java/com/umc/pureum/domain/use/UseDao.java
+++ src/main/java/com/umc/pureum/domain/use/UseDao.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -16,10 +17,17 @@ public class UseDao {
         return em.find(UsePhone.class, id);
     }
 
-    // 사용자의 외래키를 통한 사용 테이블 단건 조회
+    // 사용 테이블의 외래키를 통한 최근에 생성된 사용 테이블 단건 조회
     public UsePhone findOneByFk(Long user_id){
-        return em.createQuery("select u from UsePhone u where u.user.id = :user_id", UsePhone.class)
+        return em.createQuery("select u from UsePhone u where u.user.id = :user_id order by u.created_at desc limit 1", UsePhone.class)
                 .setParameter("user_id",user_id)
                 .getSingleResult();
+    }
+
+    // 사용 테이블의 외래키를 통한 사용 테이블 모두 조회
+    public List<UsePhone> findAll(Long user_id){
+        return em.createQuery("select u from UsePhone u where u.user.id = :user_id", UsePhone.class)
+                .setParameter("user_id",user_id)
+                .getResultList();
     }
  }
