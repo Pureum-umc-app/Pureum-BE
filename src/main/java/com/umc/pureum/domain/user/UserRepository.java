35f686164111b07ecb2c3899e3d6447a63303bbc
joeun-01 <jjy020121@gmail.com>
2023-01-20 06:38:51 +0900

    feat : fix error

--- src/main/java/com/umc/pureum/domain/user/UserRepository.java
+++ src/main/java/com/umc/pureum/domain/user/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserAccount, Long> {
-    Optional<UserAccount> findByIdAndStatus(Long id);
+    Optional<UserAccount> findByIdAndStatus(Long id, String status);
 }
