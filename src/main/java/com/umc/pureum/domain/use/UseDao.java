bdc503519c8500c8dd1380214798e33056bc682d
junwon1997 <junwon20170339@gmail.com>
2023-01-19 19:40:17 +0900

    chore: 일일 사용 시간, 휴대폰 켠 횟수 저장 api 수정

--- src/main/java/com/umc/pureum/domain/use/UseDao.java
+++ src/main/java/com/umc/pureum/domain/use/UseDao.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,10 +19,19 @@ public class UseDao {
         return em.find(Use.class, id);
     }
 
-    // 사용 테이블의 외래키를 통한 사용 테이블 단건 조회
+    // 사용 테이블의 외래키를 통한 최근에 생성된 사용 테이블 단건 조회
     public Use findOneByFk(Long user_id){
-        return em.createQuery("select u from Use u where u.user.id = :user_id", Use.class)
+        return em.createQuery("select u from Use u where u.user.id = :user_id order by u.created_at desc limit 1", Use.class)
                 .setParameter("user_id",user_id)
                 .getSingleResult();
     }
+
+    // 사용 테이블 모두 조회
+    public List<Use> findAll() {
+        return em.createQuery("select u from Use u", Use.class)
+                .getResultList();
+    }
+
+
+
  }
