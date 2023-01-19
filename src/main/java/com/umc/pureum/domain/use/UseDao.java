10026e277c81a3de005d4e0813f24e4f96266211
joeun-01 <jjy020121@gmail.com>
2023-01-20 04:56:42 +0900

    feat : [목표 달성 여부 반환 API] complete

--- src/main/java/com/umc/pureum/domain/use/UseDao.java
+++ src/main/java/com/umc/pureum/domain/use/UseDao.java
@@ -1,10 +1,8 @@
 package com.umc.pureum.domain.use;
 
-import com.umc.pureum.domain.use.dto.PostUseTimeAndCountReq;
-import com.umc.pureum.domain.use.entity.Use;
+import com.umc.pureum.domain.use.entity.UsePhone;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
@@ -14,13 +12,13 @@ public class UseDao {
     private final EntityManager em;
 
     // 사용 테이블 단건 조회
-    public Use findOne(Long id){
-        return em.find(Use.class, id);
+    public UsePhone findOne(Long id){
+        return em.find(UsePhone.class, id);
     }
 
     // 사용자의 외래키를 통한 사용 테이블 단건 조회
-    public Use findOneByFk(Long user_id){
-        return em.createQuery("select u from Use u where u.user.id = :user_id", Use.class)
+    public UsePhone findOneByFk(Long user_id){
+        return em.createQuery("select u from UsePhone u where u.user.id = :user_id", UsePhone.class)
                 .setParameter("user_id",user_id)
                 .getSingleResult();
     }
