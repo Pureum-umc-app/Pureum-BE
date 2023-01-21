82ff36f1ce8ff7df1b8e3558a1dd7d86fb8d21da
peter <97747863+wcorn@users.noreply.github.com>
2023-01-22 01:11:53 +0900

    fix: signup api completed

--- src/main/java/com/umc/pureum/domain/user/UserRepository.java
+++ src/main/java/com/umc/pureum/domain/user/UserRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserAccount, Long> {
 
     boolean existsByNickname(String nickname);
+    boolean existsByKakaoId(Long kakaoId);
 }
