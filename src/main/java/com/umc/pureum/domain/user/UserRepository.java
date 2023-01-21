ff17e6c8b6b94dd66916c9c4d56c86d0da059e7d
peter <97747863+wcorn@users.noreply.github.com>
2023-01-21 15:36:23 +0900

    feat: Modification of user entity, membership registration (profile photo not completed)

--- src/main/java/com/umc/pureum/domain/user/UserRepository.java
+++ src/main/java/com/umc/pureum/domain/user/UserRepository.java
@@ -3,5 +3,9 @@ package com.umc.pureum.domain.user;
 import com.umc.pureum.domain.user.entity.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserAccount, Long> {
+
+    boolean existsByNickname(String nickname);
 }
