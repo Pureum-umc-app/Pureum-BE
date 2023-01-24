8ffc313080fd13a33b446d863ef9d8c774e29413
joeun-01 <jjy020121@gmail.com>
2023-01-25 00:19:15 +0900

    feat : complete add meaning

--- src/main/java/com/umc/pureum/domain/sentence/SentenceController.java
+++ src/main/java/com/umc/pureum/domain/sentence/SentenceController.java
@@ -2,12 +2,13 @@ package com.umc.pureum.domain.sentence;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.DeserializationConfig;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.umc.pureum.domain.sentence.dto.GetBeforeKeywordRes;
+import com.umc.pureum.domain.sentence.entity.Word;
 import com.umc.pureum.domain.sentence.openapi.GetMeansReq;
 import com.umc.pureum.domain.sentence.openapi.GetMeansRes;
+import com.umc.pureum.domain.sentence.repository.WordRepository;
 import com.umc.pureum.global.config.BaseException;
 import com.umc.pureum.global.config.BaseResponse;
 import com.umc.pureum.global.utils.JwtService;
@@ -44,11 +45,13 @@ public class SentenceController {
     private final SentenceProvider sentenceProvider;
     private final SentenceService sentenceService;
     private final JwtService jwtService;
+    private final WordRepository wordRepository;
 
     /**
      * 한국어 기초 사전 API 연동
      * 한국어 기초 사전 API를 이용하여 단어의 뜻을 받아옴
      * 뜻은 가장 첫 번째로 나오는 뜻을 사용
+     * 서버에서 데이터를 입력할 때 사용한 코드로 프론트에는 공개되지 않음
      */
     @ApiIgnore
     @GetMapping("/means")
@@ -56,35 +59,49 @@ public class SentenceController {
         String baseUrl = "https://krdict.korean.go.kr/api/search";
 
         String key = "AF01E186D8067C9F49C4AB17E41B2B39";
-        GetMeansReq req = new GetMeansReq(key, "나무");
 
         try {
-            StringBuilder result = new StringBuilder();
+            List<Word> words = wordRepository.findAll();
 
-            // URL 설정
-            URL url = new URL(baseUrl + req.getParameter());
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            for (Word word : words) {
+                String now = word.getWord();
+                GetMeansReq req = new GetMeansReq(key, now);
+                StringBuilder result = new StringBuilder();
 
-            // 접속
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
+                // URL 설정
+                URL url = new URL(baseUrl + req.getParameter());
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(urlConnection.getInputStream());
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(bufferedInputStream, StandardCharsets.UTF_8));
-            String returnLine;
-            while((returnLine = bufferedReader.readLine()) != null) {
-                result.append(returnLine);
+                // 접속
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(bufferedInputStream, StandardCharsets.UTF_8));
+                String returnLine;
+                while ((returnLine = bufferedReader.readLine()) != null) {
+                    result.append(returnLine);
+                }
+
+                // JSON으로 변환
+                JSONObject jsonObject = XML.toJSONObject(result.toString());
+
+                ObjectMapper mapper = new ObjectMapper().configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
+                mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+                mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+
+                // GetMeansRes에 매핑
+                GetMeansRes getMeansRes = mapper.readValue(jsonObject.toString(), GetMeansRes.class);
+                System.out.println(getMeansRes);
+
+                if(getMeansRes.getChannel().getItem().isEmpty()) continue;
+
+                // DB에 뜻 저장
+                String meaning = getMeansRes.getChannel().getItem().get(0).getSense().get(0).getDefinition();
+                word.setMeaning(meaning);
+
+                wordRepository.save(word);
             }
-
-            JSONObject jsonObject = XML.toJSONObject(result.toString());
-
-            ObjectMapper mapper = new ObjectMapper().configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
-            mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-            mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-
-            GetMeansRes getMeansRes = mapper.readValue(jsonObject.toString(), GetMeansRes.class);
-
-            System.out.println(getMeansRes);
 
             return new BaseResponse<>("성공");
         } catch (Exception exception) {
@@ -99,7 +116,7 @@ public class SentenceController {
      * 작성 전 단어 리스트 반환
      * [GET] /sentences/before
      */
-    @ApiOperation("도장 개수 반환 API")
+    @ApiOperation("오늘의 작성 전 단어 반환 API")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "X-ACCESS-TOKEN", required = true, dataType = "string", paramType = "header"),
     })
