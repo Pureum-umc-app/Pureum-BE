1db3b44e03911099e41cfe98bf0b55f6891f353f
joeun-01 <jjy020121@gmail.com>
2023-01-24 16:27:19 +0900

    feat : connect 한국어 기초 사전 API

---
+++ src/main/java/com/umc/pureum/domain/sentence/dto/GetMeansReq.java
@@ -0,0 +1,30 @@
+package com.umc.pureum.domain.sentence.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class GetMeansReq {
+    private String key;
+    private String q;
+    private String part;
+    private String translated;
+    private String method;
+
+    public GetMeansReq(String key, String q) {
+        this.key = key;
+        this.q = q;
+        this.part = "word";
+        this.translated = "n";
+        this.method = "exact";
+    }
+
+    public String getParameter() {
+        return "?key=" + this.key +
+                "&q=" + this.q +
+                "&part=" + this.part +
+                "&translated=" + this.translated +
+                "&method=" + this.method;
+    }
+}
