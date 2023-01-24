1f09dbf44a36d0cf97e0f0bc9e24760f8cd8a1e6
joeun-01 <jjy020121@gmail.com>
2023-01-24 16:27:19 +0900

    feat : connect 한국어 기초 사전 API

--- src/main/java/com/umc/pureum/domain/sentence/SentenceProvider.java
+++ src/main/java/com/umc/pureum/domain/sentence/SentenceProvider.java
@@ -1,12 +1,22 @@
 package com.umc.pureum.domain.sentence;
 
+import com.umc.pureum.domain.sentence.dto.GetBeforeKeywordRes;
 import com.umc.pureum.global.utils.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class SentenceProvider {
     private final SentenceDao sentenceDao;
     private final JwtService jwtService;
+
+    public List<GetBeforeKeywordRes> getBeforeKeyword(Long id) {
+        List<GetBeforeKeywordRes> getBeforeKeywordRes = new ArrayList<>();
+
+        return getBeforeKeywordRes;
+    }
 }
