b20aa4aeb7a87baaf38dd2fd29ad3c44ec491972
joeun-01 <jjy020121@gmail.com>
2023-01-24 17:29:45 +0900

    feat : change to json value

---
+++ src/main/java/com/umc/pureum/domain/sentence/openapi/GetMeansRes.java
@@ -0,0 +1,10 @@
+package com.umc.pureum.domain.sentence.openapi;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class GetMeansRes {
+    private Channel channel;
+}
