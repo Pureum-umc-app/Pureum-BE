53249807ee15da576b7c4f57c867658438bfea10
joeun-01 <jjy020121@gmail.com>
2023-01-24 16:27:19 +0900

    feat : connect 한국어 기초 사전 API

--- src/main/java/com/umc/pureum/domain/sentence/SentenceController.java
+++ src/main/java/com/umc/pureum/domain/sentence/SentenceController.java
@@ -2,6 +2,7 @@ package com.umc.pureum.domain.sentence;
 
 import com.umc.pureum.domain.attendance.dto.GetStampRes;
 import com.umc.pureum.domain.sentence.dto.GetBeforeKeywordRes;
+import com.umc.pureum.domain.sentence.dto.GetMeansReq;
 import com.umc.pureum.global.config.BaseException;
 import com.umc.pureum.global.config.BaseResponse;
 import com.umc.pureum.global.utils.JwtService;
@@ -9,12 +10,21 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.models.media.XML;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Objects;
 
@@ -28,6 +38,46 @@ public class SentenceController {
     private final SentenceProvider sentenceProvider;
     private final SentenceService sentenceService;
     private final JwtService jwtService;
+
+    /**
+     * 한국어 기초 사전 API 연동
+     * 한국어 기초 사전 API를 이용하여 단어의 뜻을 받아옴
+     * 뜻은 가장 첫 번째로 나오는 뜻을 사용
+     */
+    @ApiIgnore
+    @GetMapping("/means")
+    public BaseResponse<String> getMeans() {
+        String baseUrl = "https://krdict.korean.go.kr/api/search";
+
+        String key = "AF01E186D8067C9F49C4AB17E41B2B39";
+        GetMeansReq req = new GetMeansReq(key, "나무");
+
+        try {
+            StringBuilder result = new StringBuilder();
+
+            // URL 설정
+            URL url = new URL(baseUrl + req.getParameter());
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            // 접속
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+
+            String returnLine;
+            result.append("<xmp>");
+            while((returnLine = bufferedReader.readLine()) != null) {
+                result.append(returnLine).append("\n");
+            }
+
+            System.out.println(result);
+
+            return new BaseResponse<>("성공");
+        } catch (Exception exception) {
+            return new BaseResponse<>(exception.getMessage());
+        }
+    }
 
 
     /**
