53fde95adb68c38766022c052cafe085cb3eb992
joeun-01 <jjy020121@gmail.com>
2023-01-26 22:56:13 +0900

    feat : [오늘의 작성 완료 단어 반환 API] create

--- src/main/java/com/umc/pureum/domain/sentence/dto/GetKeywordRes.java
+++ src/main/java/com/umc/pureum/domain/sentence/dto/GetKeywordRes.java
@@ -7,15 +7,18 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class GetBeforeKeywordRes {
+public class GetKeywordRes {
     @ApiModelProperty(example = "2023-01-22")
     private String date;
     @ApiModelProperty(example = "5")
     private String keyword;
+    @ApiModelProperty(example = "어느 한 곳에서 멀리 떨어져 있지 않다.")
+    private String meaning;
 
     @Builder
-    public GetBeforeKeywordRes(String date, String keyword) {
+    public GetKeywordRes(String date, String keyword, String meaning) {
         this.date = date;
         this.keyword = keyword;
+        this.meaning = keyword;
     }
 }
--- src/main/java/com/umc/pureum/domain/sentence/repository/KeywordRepository.java
+++ src/main/java/com/umc/pureum/domain/sentence/repository/KeywordRepository.java
@@ -2,9 +2,14 @@ package com.umc.pureum.domain.sentence.repository;
 
 import com.umc.pureum.domain.sentence.entity.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,5 +18,11 @@ public interface KeywordRepository extends JpaRepository<Keyword, Long> {
     /* 중복 검사 */
     Optional<Keyword> findByWordId(Long id);
 
-    /* 오늘의 작성 전 단어 반환 API */
+    /* 오늘의 작성 완료 단어 반환 API */
+    @Query("select k from Keyword as k \n" +
+            "   left join Sentence as s on s.keyword.id = k.id and s.user.id= :userIdx \n" +
+            "where date(k.createdAt) = :today \n" +
+            "   and k.status = 'A' \n" +
+            "   and s.status = 'O'")
+    List<Keyword> findCompleteKeyword(@Param("today") Date today, @Param("userIdx") Long userIdx);
 }
