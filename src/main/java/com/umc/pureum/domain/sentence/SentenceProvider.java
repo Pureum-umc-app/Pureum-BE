8fa18f9a1ec3c414c32aa0a721b02962f7040758
joeun-01 <jjy020121@gmail.com>
2023-01-26 22:56:13 +0900

    feat : [오늘의 작성 완료 단어 반환 API] create

--- src/main/java/com/umc/pureum/domain/sentence/SentenceProvider.java
+++ src/main/java/com/umc/pureum/domain/sentence/SentenceProvider.java
@@ -1,22 +1,61 @@
 package com.umc.pureum.domain.sentence;
 
-import com.umc.pureum.domain.sentence.dto.GetBeforeKeywordRes;
-import com.umc.pureum.global.utils.JwtService;
+import com.umc.pureum.domain.sentence.dto.GetKeywordRes;
+import com.umc.pureum.domain.sentence.entity.Keyword;
+import com.umc.pureum.domain.sentence.repository.KeywordRepository;
+import com.umc.pureum.domain.sentence.repository.SentenceRepository;
+import com.umc.pureum.domain.use.UseProvider;
+import com.umc.pureum.domain.user.UserRepository;
+import com.umc.pureum.global.config.BaseException;
+import com.umc.pureum.global.config.BaseResponseStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class SentenceProvider {
-    private final SentenceDao sentenceDao;
-    private final JwtService jwtService;
+    private final KeywordRepository keywordRepository;
+    private final SentenceRepository sentenceRepository;
+    private final UserRepository userRepository;
+    private final UseProvider useProvider;
 
-    public List<GetBeforeKeywordRes> getBeforeKeyword(Long id) {
-        List<GetBeforeKeywordRes> getBeforeKeywordRes = new ArrayList<>();
+    /* 오늘의 작성 전 단어 받아오기 */
+    public List<GetKeywordRes> getInCompleteKeyword(Long id) throws BaseException {
+        if(userRepository.findByIdAndStatus(id, "A").isEmpty()) {
+            throw new BaseException(BaseResponseStatus.INVALID_USER);
+        }
 
-        return getBeforeKeywordRes;
+        // 오늘의 작성 전 단어 받아오기
+        List<Keyword> getWords = keywordRepository.findCompleteKeyword(new Date(), id);
+        System.out.println(new Date());
+
+        return getWords.stream()
+                .map(d -> GetKeywordRes.builder()
+                        .date(useProvider.getToday(d.getCreatedAt()))
+                        .keyword(d.getWord().getWord())
+                        .meaning(d.getWord().getMeaning()).build())
+                .collect(Collectors.toList());
+    }
+
+    /* 오늘의 작성 완료 단어 받아오기 */
+    public List<GetKeywordRes> getCompleteKeyword(Long id) throws BaseException {
+        if(userRepository.findByIdAndStatus(id, "A").isEmpty()) {
+            throw new BaseException(BaseResponseStatus.INVALID_USER);
+        }
+
+        // 오늘의 작성 완료 단어 받아오기
+        List<Keyword> getWords = keywordRepository.findCompleteKeyword(new Date(), id);
+        System.out.println(new Date());
+
+        return getWords.stream()
+                .map(d -> GetKeywordRes.builder()
+                        .date(useProvider.getToday(d.getCreatedAt()))
+                        .keyword(d.getWord().getWord())
+                        .meaning(d.getWord().getMeaning()).build())
+                .collect(Collectors.toList());
     }
 }
