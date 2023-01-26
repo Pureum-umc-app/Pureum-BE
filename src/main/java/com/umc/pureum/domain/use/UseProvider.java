44c8609811278dbcd36dc82b180fcb5fa3af6c0c
joeun-01 <jjy020121@gmail.com>
2023-01-27 04:49:27 +0900

    feat : [목표 달성 여부 반환 API] add userId at response

--- src/main/java/com/umc/pureum/domain/use/UseProvider.java
+++ src/main/java/com/umc/pureum/domain/use/UseProvider.java
@@ -1,6 +1,7 @@
 package com.umc.pureum.domain.use;
 
 import com.umc.pureum.domain.use.dto.GetGoalResultsRes;
+import com.umc.pureum.domain.use.dto.GoalResult;
 import com.umc.pureum.domain.use.entity.UsePhone;
 import com.umc.pureum.domain.user.UserRepository;
 import com.umc.pureum.domain.user.entity.UserAccount;
@@ -31,7 +32,7 @@ public class UseProvider {
     /** API **/
 
     /* 목표 달성 여부 반환 API */
-    public List<GetGoalResultsRes> getGoalResults(Long userId) throws BaseException {
+    public GetGoalResultsRes getGoalResults(Long userId) throws BaseException {
         // 존재하는 회원인지 검사
         Optional<UserAccount> user = userRepository.findByIdAndStatus(userId, "A");
         if(user.isEmpty()) throw new BaseException(BaseResponseStatus.INVALID_USER);
@@ -39,11 +40,14 @@ public class UseProvider {
         // 사용 기록을 받아옴
         List<UsePhone> uses = useRepository.findAllByConditions(userId, getNextDay());
 
-        return uses.stream()
-                .map(d -> GetGoalResultsRes.builder()
+        // 결과 매핑
+        GetGoalResultsRes goalResultsRes = new GetGoalResultsRes(userId, uses.stream()
+                .map(d -> GoalResult.builder()
                         .date(getYesterday(d.getUpdatedAt()))
                         .isSuccess(getSuccess(d.getUseTime(), d.getPurposeTime())).build())
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
+
+        return goalResultsRes;
     }
 
     /* 다음 날 구하기 */
