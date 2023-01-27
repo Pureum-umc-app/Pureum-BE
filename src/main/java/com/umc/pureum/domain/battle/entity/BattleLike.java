06175867fb9b3fa8dde5577a3556bdc69c6f2bec
joeun-01 <jjy020121@gmail.com>
2023-01-28 06:27:56 +0900

    fix : modify entity

--- src/main/java/com/umc/pureum/domain/battle/entity/BattleLike.java
+++ src/main/java/com/umc/pureum/domain/battle/entity/BattleLike.java
@@ -3,14 +3,13 @@ package com.umc.pureum.domain.battle.entity;
 import com.umc.pureum.domain.sentence.entity.Sentence;
 import com.umc.pureum.domain.user.entity.UserAccount;
 import com.umc.pureum.global.entity.BaseEntity;
+import com.umc.pureum.global.entity.Status;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Data
 @SuperBuilder
@@ -20,10 +19,12 @@ public class BattleLike extends BaseEntity {
     @ManyToOne @JoinColumn(name = "battle_sentence_id")
     private BattleSentence sentence;
     @ManyToOne @JoinColumn(name = "user_id")
-    private UserAccount user;    private String status;
+    private UserAccount user;
+    @Enumerated(EnumType.STRING)
+    private Status status;
 
     @Builder
-    public BattleLike(UserAccount user, BattleSentence sentence, String status) {
+    public BattleLike(UserAccount user, BattleSentence sentence, Status status) {
         this.user = user;
         this.sentence = sentence;
         this.status = status;
