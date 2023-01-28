b979ea5d9ff84cbd12785da29c7249606197cbb7
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2023-01-28 16:07:44 +0900

    feat : [문장 좋아요 수정 API] 문장 좋아요 수정 API 추가

--- src/main/java/com/umc/pureum/domain/sentence/SentenceLikeDao.java
+++ src/main/java/com/umc/pureum/domain/sentence/SentenceLikeDao.java
@@ -1,11 +1,13 @@
 package com.umc.pureum.domain.sentence;
 
+import com.umc.pureum.domain.sentence.entity.Keyword;
 import com.umc.pureum.domain.sentence.entity.Sentence;
 import com.umc.pureum.domain.sentence.entity.SentenceLike;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -21,5 +23,13 @@ public class SentenceLikeDao {
     //문장 좋아요 단건 조회
     public SentenceLike findOne(Long id){
         return em.find(SentenceLike.class, id);
+    }
+
+
+    // sentence_id 로 sentenceLike 찾기
+    public SentenceLike findBySentenceId(Long sentenceId){
+        return em.createQuery("select s from SentenceLike s where s.sentence.id= :sentenceId",SentenceLike.class)
+                .setParameter("sentenceId", sentenceId)
+                .getSingleResult();
     }
 }
