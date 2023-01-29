3d520a9d02ee9b4e7711fd7af0a36d20ede86f9d
joeun-01 <jjy020121@gmail.com>
2023-01-30 02:32:42 +0900

    feat : [오늘의 작성 전 & 완료 단어 반환 API] add ApiResponse for swagger

--- src/main/java/com/umc/pureum/domain/sentence/SentenceController.java
+++ src/main/java/com/umc/pureum/domain/sentence/SentenceController.java
@@ -15,10 +15,7 @@ import com.umc.pureum.domain.user.service.KakaoService;
 import com.umc.pureum.domain.user.service.UserService;
 import com.umc.pureum.global.config.BaseException;
 import com.umc.pureum.global.config.BaseResponse;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONObject;
 import org.json.XML;
@@ -124,6 +121,11 @@ public class SentenceController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", paramType = "header", value = "서비스 자체 jwt 토큰")
     })
+    @ApiResponses({
+            @ApiResponse(code = 1000, message = "요청에 성공하였습니다."),
+            @ApiResponse(code = 2002, message = "유효하지 않은 JWT입니다."),
+            @ApiResponse(code = 2004, message = "존재하지 않는 유저입니다.")
+    })
     @ResponseBody
     @GetMapping("/incomplete/{userId}")
     public BaseResponse<List<GetKeywordRes>> getIncompleteKeyWords(@PathVariable Long userId) {
@@ -153,6 +155,11 @@ public class SentenceController {
     @ApiOperation("오늘의 작성 완료 단어 반환 API")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", paramType = "header", value = "서비스 자체 jwt 토큰")
+    })
+    @ApiResponses({
+            @ApiResponse(code = 1000, message = "요청에 성공하였습니다."),
+            @ApiResponse(code = 2002, message = "유효하지 않은 JWT입니다."),
+            @ApiResponse(code = 2004, message = "존재하지 않는 유저입니다.")
     })
     @ResponseBody
     @GetMapping("/complete/{userId}")
