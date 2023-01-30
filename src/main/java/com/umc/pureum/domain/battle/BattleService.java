ef189b6dd5f1b9edeaeb01d5eb329021820b1a74
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2023-01-30 16:39:27 +0900

    feat : [대결 상태 API] 대결 상태 API 추가

--- src/main/java/com/umc/pureum/domain/battle/BattleService.java
+++ src/main/java/com/umc/pureum/domain/battle/BattleService.java
@@ -1,5 +1,14 @@
 package com.umc.pureum.domain.battle;
 
+import com.umc.pureum.domain.battle.dto.BattleStatusReq;
+import com.umc.pureum.domain.battle.dto.BattleStatusRes;
+import com.umc.pureum.domain.battle.entity.Battle;
+import com.umc.pureum.domain.battle.entity.BattleStatus;
+import com.umc.pureum.domain.sentence.dto.LikeSentenceReq;
+import com.umc.pureum.domain.sentence.dto.LikeSentenceRes;
+import com.umc.pureum.domain.sentence.entity.Sentence;
+import com.umc.pureum.domain.sentence.entity.SentenceLike;
+import com.umc.pureum.domain.user.entity.UserAccount;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,4 +19,46 @@ import org.springframework.transaction.annotation.Transactional;
 public class BattleService {
     private final BattleDao battleDao;
     private final BattleProvider battleProvider;
+
+    // accept : 대결 수락
+    @Transactional
+    public BattleStatusRes accept(BattleStatusReq request) {
+
+        // request 로 받은 battleId 로 battle 찾기
+        Battle battle = battleDao.findOne(request.getBattleId());
+
+        // battle 상태를 수락 완료로 바꾸기
+        battle.setStatus(BattleStatus.A);
+
+        // battle ID , Status 값
+        return new BattleStatusRes(battle.getId(), battle.getStatus());
+    }
+
+    // reject : 대결 거절
+    @Transactional
+    public BattleStatusRes reject(BattleStatusReq request) {
+
+        // request 로 받은 battleId 로 battle 찾기
+        Battle battle = battleDao.findOne(request.getBattleId());
+
+        // battle 상태를 수락 완료로 바꾸기
+        battle.setStatus(BattleStatus.D);
+
+        // battle ID , Status 값
+        return new BattleStatusRes(battle.getId(), battle.getStatus());
+    }
+
+    // cancel : 대결 취소
+    @Transactional
+    public BattleStatusRes cancel(BattleStatusReq request) {
+
+        // request 로 받은 battleId 로 battle 찾기
+        Battle battle = battleDao.findOne(request.getBattleId());
+
+        // battle 상태를 수락 완료로 바꾸기
+        battle.setStatus(BattleStatus.D);
+
+        // battle ID , Status 값
+        return new BattleStatusRes(battle.getId(), battle.getStatus());
+    }
 }
