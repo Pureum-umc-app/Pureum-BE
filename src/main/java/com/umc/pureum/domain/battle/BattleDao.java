0d20b5ba1451d35a51a3ada02d5e4573b74992b1
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2023-01-30 16:39:27 +0900

    feat : [대결 상태 API] 대결 상태 API 추가

--- src/main/java/com/umc/pureum/domain/battle/BattleDao.java
+++ src/main/java/com/umc/pureum/domain/battle/BattleDao.java
@@ -1,7 +1,20 @@
 package com.umc.pureum.domain.battle;
 
+import com.umc.pureum.domain.battle.entity.Battle;
+import com.umc.pureum.domain.sentence.entity.Sentence;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+
 @Repository
+@RequiredArgsConstructor
 public class BattleDao {
+
+    private final EntityManager em;
+
+    //대결 단건 조회
+    public Battle findOne(Long id){
+        return em.find(Battle.class, id);
+    }
 }
---
+++ src/main/java/com/umc/pureum/domain/battle/dto/BattleStatusRes.java
@@ -0,0 +1,23 @@
+package com.umc.pureum.domain.battle.dto;
+
+import com.umc.pureum.domain.battle.entity.BattleStatus;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+
+public class BattleStatusRes {
+    @ApiModelProperty(example = "5")
+    private Long battleId;
+
+    @ApiModelProperty(example = "A")
+    private BattleStatus battleStatus;
+
+    @Builder
+    public BattleStatusRes(Long battleId , BattleStatus battleStatus){
+    }
+
+}
