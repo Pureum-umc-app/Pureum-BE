734f03c66e58e445144c302ff477b46bb0c49b39
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2023-01-31 00:59:21 +0900

    feat : [대결 받은 사람 대결 문장 작성 API] 대결 받은 사람 대결 문장 작성 API 추가

---
+++ src/main/java/com/umc/pureum/domain/battle/dto/CreateChallengedSentenceRes.java
@@ -0,0 +1,30 @@
+package com.umc.pureum.domain.battle.dto;
+
+import com.umc.pureum.domain.battle.entity.BattleStatus;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+
+public class CreateChallengedSentenceRes {
+    @ApiModelProperty(example = "5")
+    private Long battleSentenceId;
+
+    @ApiModelProperty(example = "5")
+    private Long battleId;
+
+    @ApiModelProperty(example = "A")
+    private BattleStatus battleStatus;
+
+    @Builder
+    public CreateChallengedSentenceRes(Long battleSentenceId ,Long battleId , BattleStatus battleStatus){
+        this.battleSentenceId = battleSentenceId;
+        this.battleId = battleId;
+        this.battleStatus = battleStatus;
+    }
+
+}
