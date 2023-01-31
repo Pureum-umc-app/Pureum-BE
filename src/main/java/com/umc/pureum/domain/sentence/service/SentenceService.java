6fc1f17b2fd1235ba602f99a3a62c8ceba960047
peter <97747863+wcorn@users.noreply.github.com>
2023-01-31 21:59:51 +0900

    feat: add 단어별 문장 리스트 반환 API

--- src/main/java/com/umc/pureum/domain/sentence/service/SentenceService.java
+++ src/main/java/com/umc/pureum/domain/sentence/service/SentenceService.java
@@ -1,14 +1,20 @@
-package com.umc.pureum.domain.sentence;
+package com.umc.pureum.domain.sentence.service;
 
+import com.umc.pureum.domain.sentence.SentenceDao;
+import com.umc.pureum.domain.sentence.SentenceLikeDao;
 import com.umc.pureum.domain.sentence.dto.CreateSentenceReq;
 import com.umc.pureum.domain.sentence.dto.CreateSentenceRes;
 import com.umc.pureum.domain.sentence.dto.LikeSentenceReq;
 import com.umc.pureum.domain.sentence.dto.LikeSentenceRes;
+import com.umc.pureum.domain.sentence.dto.response.SentenceListRes;
 import com.umc.pureum.domain.sentence.entity.Keyword;
 import com.umc.pureum.domain.sentence.entity.Sentence;
 import com.umc.pureum.domain.sentence.entity.SentenceLike;
 import com.umc.pureum.domain.sentence.entity.Word;
+import com.umc.pureum.domain.sentence.entity.mapping.SentenceLikeMapping;
+import com.umc.pureum.domain.sentence.function.TimeGeneralization;
 import com.umc.pureum.domain.sentence.repository.KeywordRepository;
+import com.umc.pureum.domain.sentence.repository.SentenceRepository;
 import com.umc.pureum.domain.sentence.repository.WordRepository;
 import com.umc.pureum.domain.user.UserRepository;
 import com.umc.pureum.domain.user.entity.UserAccount;
@@ -30,6 +36,9 @@ import static com.umc.pureum.global.config.BaseResponseStatus.POST_SENTENCE_NO_E
 @Transactional(readOnly = true)
 @Service
 public class SentenceService {
+    private final SentenceLikeService sentenceLikeService;
+
+    private final SentenceRepository sentenceRepository;
     private final SentenceDao sentenceDao;
     private final SentenceLikeDao sentenceLikeDao;
     private final WordRepository wordRepository;
@@ -38,7 +47,7 @@ public class SentenceService {
 
     // write : 작성한 문장 DB 에 저장
     @Transactional
-    public CreateSentenceRes write(Long userId , CreateSentenceReq request) throws BaseException{
+    public CreateSentenceRes write(Long userId, CreateSentenceReq request) throws BaseException {
 
         String writingSentence = request.getSentence();
         Long keywordId = request.getKeywordId();
@@ -50,21 +59,20 @@ public class SentenceService {
         String writingWord = word.getWord();
 
         // 작성한 문장 존재 여부 확인
-        if(writingSentence == ""){
+        if (writingSentence == "") {
             throw new BaseException(POST_SENTENCE_EMPTY);
         }
 
         // 작성할 문장에 단어 포함 여부 확인
-        if(!isExist(writingSentence , writingWord)){
+        if (!isExist(writingSentence, writingWord)) {
             throw new BaseException(POST_SENTENCE_NO_EXISTS_KEYWORD);
         }
-
 
 
         // request 로 받은 userId 로 userAccount 찾기
         UserAccount userAccount = userRepository.findById(userId).get();
 
-        Sentence sentence = new Sentence(userAccount, request.getSentence(), keyword , sentenceStatus);
+        Sentence sentence = new Sentence(userAccount, request.getSentence(), keyword, sentenceStatus);
         sentenceDao.save(sentence);
 
         return new CreateSentenceRes(sentence.getId());
@@ -73,13 +81,13 @@ public class SentenceService {
 
     /* Sentence 내에 Keyword 존재여부 검사*/
     // isExist : 문장에 키워드가 포함되어있는지 확인하는 함수
-    private boolean isExist(String writingSentence , String writingWord) {
+    private boolean isExist(String writingSentence, String writingWord) {
         return writingSentence.contains(writingWord);
     }
 
     // like : 문장 좋아요 DB 에 저장
     @Transactional
-    public LikeSentenceRes like(long userId , LikeSentenceReq request) {
+    public LikeSentenceRes like(long userId, LikeSentenceReq request) {
 
         // request 로 받은 sentenceId 로 문장 찾기
         Sentence sentence = sentenceDao.findOne(request.getSentenceId());
@@ -88,15 +96,14 @@ public class SentenceService {
         UserAccount userAccount = userRepository.findById(userId).get();
 
         //request 로 받은 sentenceId 로 문장 좋아요 찾기
-        if(sentenceLikeDao.findBySentenceId(request.getSentenceId()).isPresent()){
+        if (sentenceLikeDao.findBySentenceId(request.getSentenceId()).isPresent()) {
 
             SentenceLike sentenceLike = sentenceLikeDao.findBySentenceId(request.getSentenceId()).get();
 
             // 존재하는 sentence 일 경우 sentence status 확인하고 status 바꾼다 .
-            if("A".equals(sentenceLike.getStatus())){
+            if ("A".equals(sentenceLike.getStatus())) {
                 sentenceLike.setStatus("D");
-            }
-            else if("D".equals(sentenceLike.getStatus())){
+            } else if ("D".equals(sentenceLike.getStatus())) {
                 sentenceLike.setStatus("A");
             }
 
@@ -105,7 +112,7 @@ public class SentenceService {
         }
 
         // 존재하지 않는 sentence 일 경우 sentenceLike 생성해서 저장
-        else{
+        else {
             SentenceLike sentenceLike = new SentenceLike(userAccount, sentence, "A");
             sentenceLikeDao.save(sentenceLike);
 
@@ -125,7 +132,7 @@ public class SentenceService {
         System.out.println("start");
         List<Word> words = new ArrayList<>();
 
-        while(words.size() < 3) {  // 하루에 최대 3개까지 리턴
+        while (words.size() < 3) {  // 하루에 최대 3개까지 리턴
             // 랜덤 값을 생성
             Random random = new Random();
             Long id = (long) (random.nextInt(4300) + 1);
@@ -133,17 +140,42 @@ public class SentenceService {
             // 단어를 받아옴
             Optional<Word> word = wordRepository.findById(id);
 
-            if(word.isPresent()) {
+            if (word.isPresent()) {
                 // 단어가 존재하면 keyword에 존재하는지 검사
                 Optional<Keyword> keyword = keywordRepository.findByWordId(id);
 
-                if(keyword.isEmpty()) {
+                if (keyword.isEmpty()) {
                     // Keyword 테이블에 존재하는지 검사 후 없으면 넣기
                     keywordRepository.save(new Keyword(word.get(), "A"));
                     words.add(word.get());
                 }
             }
         }
+    }
+
+    public List<SentenceListRes> getSentenceList(long userId, long word_id, int page, int limit, String sort) {
+        List<SentenceLikeMapping> sentenceLikeMappings = sentenceLikeService.getSentenceLikeOrderByDate(word_id, page, limit, sort);
+        List<SentenceListRes> sentenceListResList = new ArrayList<>();
+        TimeGeneralization timeGeneralization = new TimeGeneralization();
+        String time;
+        SentenceListRes sentenceListRes;
+        for (SentenceLikeMapping sentenceLikeMapping : sentenceLikeMappings) {
+            time = timeGeneralization.genericTime(sentenceLikeMapping.getTime());
+            sentenceListRes = SentenceListRes.builder()
+                    .likeNum(sentenceLikeMapping.getLikeNum())
+                    .sentenceId(sentenceLikeMapping.getSentence_id())
+                    .sentence(sentenceLikeMapping.getSentence())
+                    .keywordId(sentenceLikeMapping.getKeywordId())
+                    .image(sentenceLikeMapping.getImage())
+                    .keyword(sentenceLikeMapping.getKeyword())
+                    .nickname(sentenceLikeMapping.getNickname())
+                    .userId(sentenceLikeMapping.getUserId())
+                    .selfLike(sentenceLikeService.getSentenceSelfLike(userId, sentenceLikeMapping.getSentence_id()))
+                    .time(time)
+                    .build();
+            sentenceListResList.add(sentenceListRes);
+        }
+        return sentenceListResList;
     }
 }
 
