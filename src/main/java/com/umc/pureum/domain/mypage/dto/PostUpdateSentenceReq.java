24ba826860d785e7dbeb5d929d84e93dda65192f
junwon1997 <junwon20170339@gmail.com>
2023-02-01 19:08:20 +0900

    chore: builder 수정

--- src/main/java/com/umc/pureum/domain/mypage/dto/PostUpdateSentenceReq.java
+++ src/main/java/com/umc/pureum/domain/mypage/dto/PostUpdateSentenceReq.java
@@ -2,10 +2,14 @@ package com.umc.pureum.domain.mypage.dto;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class PostUpdateSentenceReq {
     private String sentence;
 }
