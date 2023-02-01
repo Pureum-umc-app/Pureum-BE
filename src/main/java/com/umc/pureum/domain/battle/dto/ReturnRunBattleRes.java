ca783f2b7e23252b2c86ba3a9fa6c027513d0746
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2023-02-01 18:11:02 +0900

    m

---
+++ src/main/java/com/umc/pureum/domain/battle/dto/ReturnRunBattleRes.java
@@ -0,0 +1,19 @@
+package com.umc.pureum.domain.battle.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ReturnRunBattleRes {
+
+    private Long battleId;
+
+    private Long challengerId;
+
+    private Long challenger
+
+    private Long challengedId;
+
+    private Long
+}
