cb459e30a87d22ff30bdaa00fe49bcd185ea29af
junwon1997 <junwon20170339@gmail.com>
2023-02-02 20:30:45 +0900

    feat: add return three battle keyword api & put three battle keyword

---
+++ src/main/java/com/umc/pureum/domain/battle/dto/GetBattleWordRes.java
@@ -0,0 +1,18 @@
+package com.umc.pureum.domain.battle.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GetBattleWordRes {
+
+    private Long wordId;
+    private String word;
+    private String meaning;
+}
