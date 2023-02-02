04115fdc6b85fefae4e9ca797a7213a5d588cc46
정조은 <jjy020121@gmail.com>
2023-02-02 23:47:04 +0900

    Revert "Hotfix"

--- src/main/java/com/umc/pureum/domain/test/TestController.java
+++ src/main/java/com/umc/pureum/domain/test/TestController.java
@@ -1,23 +1,8 @@
 package com.umc.pureum.domain.test;
 
-import com.umc.pureum.domain.sentence.SentenceDao;
-import com.umc.pureum.domain.sentence.entity.SentenceLike;
-import com.umc.pureum.domain.sentence.entity.mapping.SentenceLikeMapping;
-import com.umc.pureum.domain.sentence.repository.SentenceLikeRepository;
-import com.umc.pureum.domain.sentence.service.SentenceService;
-import com.umc.pureum.domain.user.UserRepository;
-import com.umc.pureum.global.config.BaseResponse;
-import org.hibernate.criterion.Order;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,19 +14,18 @@ import springfox.documentation.annotations.ApiIgnore;
 @RequestMapping("/test")
 public class TestController {
     final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
-    public TestController() {
-    }
+    public TestController() {}
 
     /**
      * 로그 테스트 API
      * [GET] /test/log
-     *
      * @return String
      */
     @ResponseBody
     @GetMapping("/log")
-    public ResponseEntity<BaseResponse<String>> getAll() {
+    public String getAll() {
         System.out.println("테스트");
 //        trace, debug 레벨은 Console X, 파일 로깅 X
 //        logger.trace("TRACE Level 테스트");
@@ -54,11 +38,7 @@ public class TestController {
 //        error 레벨은 Console 로깅 O, 파일 로깅 O (app.log 뿐만 아니라 error.log 에도 로깅 됨)
 //        app.log 와 error.log 는 날짜가 바뀌면 자동으로 *.gz 으로 압축 백업됨
         logger.error("ERROR Level 테스트");
-        User principal = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String UserId = principal.getUsername();
-//        System.out.println(sentenceLikeRepository.findByStatus( "A", PageRequest.of(0,1,Sort.by(Sort.Order.desc("id")))).getContent());
 
-//        sentenceLikeMappings.forEach(System.out::println);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new BaseResponse(UserId));
+        return "Success Test";
     }
 }
--- src/main/java/com/umc/pureum/global/config/BaseResponseStatus.java
+++ src/main/java/com/umc/pureum/global/config/BaseResponseStatus.java
@@ -28,8 +28,7 @@ public enum BaseResponseStatus {
     EMPTY_JWT(false, 2001, "JWT를 입력해주세요."),
     INVALID_JWT(false, 2002, "유효하지 않은 JWT입니다."),
     INVALID_USER_JWT(false,2003,"권한이 없는 유저의 접근입니다."),
-    INVALID_USER(false, 2004, "존재하지 않는 유저입니다."),
-    INVALID_IMAGE_FILE(false,2005,"이미지파일이 아닙니다"),
+
     // users
     POST_USERS_EMPTY_NICKNAME(false, 2010, "닉네임을 입력해주세요."),
     POST_USERS_EMPTY_AGE(false, 2012, "나이를 입력해주세요."),
@@ -43,19 +42,8 @@ public enum BaseResponseStatus {
     POST_USERS_EXISTS_EMAIL(false,2030,"중복된 이메일입니다."),
     POST_USERS_EXISTS_NICKNAME(false,2031,"중복된 닉네임입니다."),
     POST_USERS_NO_EXISTS_EMAIL(false,2032,"존재하지 않는 이메일입니다."),
-    POST_USERS_EXISTS(false,2033,"이미 가입된 회원입니다."),
-    POST_USERS_NO_EXISTS_USER(false,2034 ,"존재하지 않는 회원입니다."),
-    POST_USE_EXISTS_USAGE_TIME(false, 2051, "이미 목표 시간을 설정하였습니다."),
-            //sentence
-    POST_SENTENCE_EMPTY(false, 2040, "문장을 입력해주세요."),
-    POST_SENTENCE_NO_EXISTS_KEYWORD(false, 2041, "키워드가 포함되어있지 않습니다."),
-    GET_SENTENCE_INVALID_SORT_METHOD(false, 2042, "정렬 방식이 잘못되었습니다."),
 
-    // battle
-    POST_BATTLE_EMPTY_SENTENCE(false, 2050, "대결 문장을 입력해야 합니다."),
-    POST_BATTLE_NO_EXIST_KEYWORD(false, 2051,"존재하지 않는 키워드입니다."),
-    POST_BATTLE_ALREADY_EXIST_KEYWORD(false, 2052,"이미 대결에 사용한 단어입니다."),
-    GET_BATTLE_INVALID_STATUS(false, 2053,"대기 중 혹은 종료된 대결만 반환 가능합니다."),
+    // homes
 
 
 
