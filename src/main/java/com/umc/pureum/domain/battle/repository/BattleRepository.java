aaa75df0f3e847b09573a2e935b07413a7bd4e07
peter <97747863+wcorn@users.noreply.github.com>
2023-02-04 04:21:25 +0900

    hotfix: edit repository

--- src/main/java/com/umc/pureum/domain/battle/repository/BattleRepository.java
+++ src/main/java/com/umc/pureum/domain/battle/repository/BattleRepository.java
@@ -113,4 +113,11 @@ public interface BattleRepository extends JpaRepository<Battle, Long> {
             "   and b.status = 'C' \n" +
             "   and r.status = 'A'")
     List<GetCompleteBattles> findAllByComplete(Long userId);
+    @Query("select b.id as battleId, b.word.id as keywordId, b.word.word.word as keyword, \n" +
+            "   b.challenger.id as challengerId, b.challenger.nickname as challengerNickname, b.challenger.image as challengerProfileImg, \n" +
+            "   b.challenged.id as challengedId, b.challenged.nickname as challengedNickname, b.challenged.image as challengedProfileImg, \n" +
+            "   b.duration as duration, b.status as battleStatus \n" +
+            "from Battle as b \n" +
+            "where b.id = :battleId")
+    List<GetBattleInfoRes> findInfoByBattleId(@Param("battleId") Long battleId);
 }
