1103fc16cbfd42ff8480cf422abc620bcaca7e03
peter <97747863+wcorn@users.noreply.github.com>
2023-02-04 04:02:49 +0900

    feat: set battle_result add

--- src/main/java/com/umc/pureum/domain/battle/repository/BattleSentenceRepository.java
+++ src/main/java/com/umc/pureum/domain/battle/repository/BattleSentenceRepository.java
@@ -3,7 +3,10 @@ package com.umc.pureum.domain.battle.repository;
 import com.umc.pureum.domain.battle.entity.BattleSentence;
 import com.umc.pureum.global.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,4 +16,13 @@ import java.util.Optional;
 public interface BattleSentenceRepository extends JpaRepository<BattleSentence, Long> {
     /* 대결에 사용한 문장 받아오기 */
     Optional<BattleSentence> findByBattleIdAndUserIdAndStatus(Long battleId, Long userId, Status status);
+
+    @Query(nativeQuery = true,
+            value = "select id " +
+                    "from battle_sentence " +
+                    "where battle_id  = :battleId and user_id = :userId and status ='A'"
+    )
+    long findIdByBattleIdAndUserIdAndStatus(
+            @Param("battleId") Long battleId,
+            @Param("userId") Long userId);
 }
