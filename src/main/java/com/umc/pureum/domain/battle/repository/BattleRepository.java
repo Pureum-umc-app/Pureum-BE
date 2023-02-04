3b65a95c4882f32b3403fbbda95344a3b93551ec
joeun-01 <jjy020121@gmail.com>
2023-02-04 21:19:52 +0900

    feat : [나의 대기 중인 대결 리스트 반환 API] add paging

--- src/main/java/com/umc/pureum/domain/battle/repository/BattleRepository.java
+++ src/main/java/com/umc/pureum/domain/battle/repository/BattleRepository.java
@@ -71,17 +71,16 @@ public interface BattleRepository extends JpaRepository<Battle, Long> {
             "   and b.status = :status")
     List<GetBattlesInterface> findAllByUserIdAndStatus(@Param("userId") Long userId, @Param("status") BattleStatus status);
 
-    /* 대기 중인 대결 리스트 반환 */
+    /* 나의 대기 중인 대결 리스트 반환 */
     @Query("select b.id as battleId, \n" +
             "   b.challenger.id as challengerId, b.challenger.nickname as challengerNickname, b.challenger.image as challengerProfileImg, \n" +
             "   b.challenged.id as challengedId, b.challenged.nickname as challengedNickname, b.challenged.image as challengedProfileImg, \n" +
             "   b.word.id as keywordId, b.word.word.word as keyword, \n" +
             "   b.duration as duration \n" +
             "from Battle as b \n" +
-            "where b.challenged.id = :userId" +
-            "    or b.challenger.id = :userId" +
-            "    and b.status = :status")
-    List<GetWaitBattlesRes> findAllByWaitBattles(@Param("userId") Long userId, @Param("status")BattleStatus status);
+            "where (b.challenged.id = :userId or b.challenger.id = :userId) \n" +
+            "    and (b.status = 'W' or b.status = 'A')")
+    List<GetWaitBattlesRes> findAllWaitBattles(@Param("userId") Long userId, PageRequest request);
 
     /* 나의 종료된 대결 리스트 반환 */
     @Query("select b.id as battleId, b.word.id as wordId, b.word.word.word as word, \n" +
