8771acd718b4483440c357009fc33c3c02006f6a
junwon1997 <junwon20170339@gmail.com>
2023-02-05 06:30:55 +0900

    feat: add 배지 저장 api

--- src/main/java/com/umc/pureum/domain/badge/entity/Badge.java
+++ src/main/java/com/umc/pureum/domain/badge/entity/Badge.java
@@ -4,6 +4,7 @@ package com.umc.pureum.domain.badge.entity;
 import com.umc.pureum.domain.user.entity.UserAccount;
 import com.umc.pureum.global.entity.BaseEntity;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
@@ -16,6 +17,7 @@ import javax.persistence.ManyToOne;
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
+@Data
 public class Badge extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -25,4 +27,9 @@ public class Badge extends BaseEntity {
     private int badge;
 
     private String status;
+
+    public Badge(UserAccount userAccount, int badge){
+        this.userAccount = userAccount;
+        this.badge = badge;
+    }
 }
