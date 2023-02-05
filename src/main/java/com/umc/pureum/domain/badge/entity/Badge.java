546a0a181c307be409bb33f5cd13f77cb7553cb7
peter <97747863+wcorn@users.noreply.github.com>
2023-02-06 06:03:37 +0900

    hotfix: badge entitiy edit

--- src/main/java/com/umc/pureum/domain/badge/entity/Badge.java
+++ src/main/java/com/umc/pureum/domain/badge/entity/Badge.java
@@ -5,6 +5,7 @@ import com.umc.pureum.domain.user.entity.UserAccount;
 import com.umc.pureum.global.entity.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
@@ -12,24 +13,21 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@SuperBuilder
 @Data
+@SuperBuilder
+@NoArgsConstructor
+@Entity
 public class Badge extends BaseEntity {
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private UserAccount userAccount;
+    @ManyToOne @JoinColumn(name = "user_id")
+    private UserAccount user;
 
     private int badge;
 
     private String status;
 
-    public Badge(UserAccount userAccount, int badge){
-        this.userAccount = userAccount;
+    public Badge(UserAccount user, int badge){
+        this.user = user;
         this.badge = badge;
     }
 }
