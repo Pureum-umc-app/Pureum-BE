a9c0006b3dac6f5710ab164e1662701cf6f3b277
joeun-01 <jjy020121@gmail.com>
2023-02-06 21:36:03 +0900

    fix : modify return waiting battles response

--- src/main/java/com/umc/pureum/domain/battle/dto/response/GetWaitBattlesRes.java
+++ src/main/java/com/umc/pureum/domain/battle/dto/response/GetWaitBattlesRes.java
@@ -2,13 +2,10 @@ package com.umc.pureum.domain.battle.dto.response;
 
 public interface GetWaitBattlesRes {
     Long getBattleId();
-    Long getChallengerId();
-    String getChallengerNickname();
-    String getChallengerProfileImg();
-    Long getChallengedId();
-    String getChallengedNickname();
-    String getChallengedProfileImg();
-    Long getKeywordId();
-    String getKeyword();
+    Long getOtherId();
+    String getOtherNickname();
+    String getOtherProfileImg();
+    Long getWordId();
+    String getWord();
     int getDuration();
 }
