553cf15495fcf674c78b71227a6c64da599e356a
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2023-02-08 19:22:11 +0900

    feat : 예외처리

--- src/main/java/com/umc/pureum/domain/battle/dto/response/ReturnRunBattleRes.java
+++ src/main/java/com/umc/pureum/domain/battle/dto/response/ReturnRunBattleRes.java
@@ -1,12 +1,14 @@
 package com.umc.pureum.domain.battle.dto.response;
 
 import com.umc.pureum.domain.battle.entity.BattleStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class ReturnRunBattleRes {
 
     private Long battleId;
@@ -33,34 +35,4 @@ public class ReturnRunBattleRes {
     private int selfLike;
     private int oppLike;
 
-    @Builder
-
-    public ReturnRunBattleRes(Long battleId, Long keywordId, String keyword, String remainDuration,
-                              Long challengedId, String challengedNickname, String challengedImage,
-                              Long challengerId, String challengerNickname, String challengerImage,
-                              int duration, BattleStatus status,
-                              Long challengedSentenceId, String challengedSentence,
-                              Long challengerSentenceId, String challengerSentence,
-                              Long challengedLikeCnt, Long challengerLikeCnt, int selfLike, int oppLike) {
-        this.battleId = battleId;
-        this.keywordId = keywordId;
-        this.keyword = keyword;
-        this.remainDuration = remainDuration;
-        this.challengedId = challengedId;
-        this.challengedNickname = challengedNickname;
-        this.challengedImage = challengedImage;
-        this.challengerId = challengerId;
-        this.challengerNickname = challengerNickname;
-        this.challengerImage = challengerImage;
-        this.duration = duration;
-        this.status = status;
-        this.challengedSentenceId = challengedSentenceId;
-        this.challengedSentence = challengedSentence;
-        this.challengerSentenceId = challengerSentenceId;
-        this.challengerSentence = challengerSentence;
-        this.challengedLikeCnt = challengedLikeCnt;
-        this.challengerLikeCnt = challengerLikeCnt;
-        this.selfLike = selfLike;
-        this.oppLike = oppLike;
-    }
 }
