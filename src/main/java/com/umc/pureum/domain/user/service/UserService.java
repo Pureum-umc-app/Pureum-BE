278abd21f02dba549c795d7048f8c0084ef23a1d
peter <97747863+wcorn@users.noreply.github.com>
2023-02-08 14:59:20 +0900

    feat: 카카오 토큰 헤더 바디로 변경

--- src/main/java/com/umc/pureum/domain/user/service/UserService.java
+++ src/main/java/com/umc/pureum/domain/user/service/UserService.java
@@ -17,7 +17,6 @@ import com.umc.pureum.domain.use.UseRepository;
 import com.umc.pureum.domain.use.entity.UsePhone;
 import com.umc.pureum.domain.use.entity.UseStatus;
 import com.umc.pureum.domain.user.UserRepository;
-import com.umc.pureum.domain.user.dto.request.FCMDto;
 import com.umc.pureum.domain.user.dto.request.KakaoAccessTokenInfoDto;
 import com.umc.pureum.domain.user.dto.request.CreateUserDto;
 import com.umc.pureum.domain.mypage.dto.response.GetProfileResponseDto;
@@ -92,10 +91,10 @@ public class UserService {
         return userRepository.findByKakaoIdAndStatus(kakaoId, "A").getId();
     }
 
-    public LogInResponseDto userLogIn(Long id, FCMDto fcmDto) {
+    public LogInResponseDto userLogIn(Long id, String fcmId) {
         String jwt = jwtTokenProvider.createAccessToken(Long.toString(id));
         Optional<UserAccount> userAccount = userRepository.findByIdAndStatus(id, "A");
-        userAccount.get().setFcmId(fcmDto.getFcmId());
+        userAccount.get().setFcmId(fcmId);
         return new LogInResponseDto(jwt);
     }
 
