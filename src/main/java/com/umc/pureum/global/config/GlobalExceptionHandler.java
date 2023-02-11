3a28828297cdfa8f51fbacee3f3ab2748af6fbec
peter <97747863+wcorn@users.noreply.github.com>
2023-02-11 19:52:30 +0900

    hotfix: GlobalExceptionHandler 추가

---
+++ src/main/java/com/umc/pureum/global/config/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.umc.pureum.global.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BaseException.class)
+    protected ResponseEntity<BaseResponse> handleCustomException(final BaseException e) {
+        log.error("handleCustomException: {}", e.getStatus());
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new BaseResponse(e.getStatus()));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ResponseEntity<BaseResponse> handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException e) {
+        log.error("handleHttpRequestMethodNotSupportedException: {}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(new BaseResponse(BaseResponseStatus.NOT_ALLOW_METHOD));
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<BaseResponse> handleException(final Exception e) {
+        log.error("handleException: {}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new BaseResponse(BaseResponseStatus.DATABASE_ERROR));
+    }
+
+}
