eba3d1c8fd6d709fb1163957fea520ffd3945fae
joeun-01 <jjy020121@gmail.com>
2023-02-11 20:07:22 +0900

    feat : [종료된 대결 리스트 반환 API] 결과 여부 추가

--- src/main/java/com/umc/pureum/domain/battle/dto/response/GetCompleteBattles.java
+++ src/main/java/com/umc/pureum/domain/battle/dto/response/GetCompleteBattles.java
@@ -11,10 +11,12 @@ public interface GetCompleteBattles {
     String getWord();
     @ApiModelProperty("이긴 경우 : 이긴 사람 유저 인덱스, 비긴 경우 : 0")
     Long getWinnerId();
-    @ApiModelProperty("이긴 경우 : 이긴 사람 닉네임, 비긴 경우 : \"\"")
+    @ApiModelProperty("이긴 경우 : 이긴 사람 닉네임, 비긴 경우 : null")
     String getWinnerNickname();
-    @ApiModelProperty("이긴 경우 : 이긴 사람 사진, 비긴 경우 : 첫 번째 유저 사진")
+    @ApiModelProperty("이긴 경우 : 이긴 사람 사진, 비긴 경우 : 챌린저 사진")
     String getWinnerProfileImg();
-    @ApiModelProperty("이긴 경우 : \"\", 비긴 경우 : 두 번째 유저 사진")
+    @ApiModelProperty("이긴 경우 : null, 비긴 경우 : 다른 사람 사진")
     String getOtherProfileImg();
+    @ApiModelProperty("비긴 경우 : 0, 이기거나 진 경우 : 1")
+    int getHasResult();
 }
