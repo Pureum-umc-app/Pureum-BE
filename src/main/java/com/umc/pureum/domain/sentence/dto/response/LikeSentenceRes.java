2d4d6475594dff80469dffe0aba5ca85636a01bd
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2023-02-14 01:09:05 +0900

    fix : error fix

--- src/main/java/com/umc/pureum/domain/sentence/dto/response/LikeSentenceRes.java
+++ src/main/java/com/umc/pureum/domain/sentence/dto/response/LikeSentenceRes.java
@@ -1,5 +1,6 @@
 package com.umc.pureum.domain.sentence.dto.response;
 
+import com.umc.pureum.global.entity.Status;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +13,12 @@ public class LikeSentenceRes {
     @ApiModelProperty(example = "5")
     private Long sentence_like_id;
 
+    @ApiModelProperty(example = "A")
+    private String status;
+
     @Builder
-    public LikeSentenceRes(Long sentence_like_id){
+    public LikeSentenceRes(Long sentence_like_id, String status) {
         this.sentence_like_id = sentence_like_id;
+        this.status = status;
     }
 }
