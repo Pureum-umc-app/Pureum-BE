84bf611e7bfbb993bde9f5b181b2f87212b110b9
junwon1997 <junwon20170339@gmail.com>
2023-02-13 21:26:41 +0900

    fix: mypage code refactoring

--- src/main/java/com/umc/pureum/domain/mypage/MyPageDao.java
+++ src/main/java/com/umc/pureum/domain/mypage/MyPageDao.java
@@ -22,21 +22,30 @@ public class MyPageDao {
 
     // 문장 테이블 외래키를 통한 문장 조회
     public List<Sentence> findByFk(Long userId){
-       return em.createQuery("select s from Sentence s where s.user.id = :userId and s.status != 'D'", Sentence.class)
+       return em.createQuery("select s from Sentence s " +
+                       "join fetch s.user u " +
+                       "join fetch s.keyword k " +
+                       "where u.id = :userId and s.status != 'D'", Sentence.class)
                 .setParameter("userId",userId)
                 .getResultList();
     }
 
     // 문장 테이블 외래키를 통한 문장 조회(조건 -> 상태가 open 인 경우만 조회)
     public List<Sentence> findByFkOnlyOpen(Long userId){
-        return em.createQuery("select s from Sentence s where s.user.id = :userId and s.status = 'O'", Sentence.class)
+        return em.createQuery("select s from Sentence s " +
+                        "join fetch s.user u " +
+                        "join fetch s.keyword k " +
+                        "where s.user.id = :userId and s.status = 'O'", Sentence.class)
                 .setParameter("userId",userId)
                 .getResultList();
     }
 
     // sentence_like 테이블 조회
     public List<SentenceLike> findSentenceLike(Long sentenceId){
-        return em.createQuery("select s from SentenceLike s where s.sentence.id = :sentenceId")
+        return em.createQuery("select s from SentenceLike s " +
+                        "join fetch s.user u " +
+                        "join fetch s.sentence ss " +
+                        "where ss.id = :sentenceId and s.status != 'D'")
                 .setParameter("sentenceId", sentenceId)
                 .getResultList();
     }
