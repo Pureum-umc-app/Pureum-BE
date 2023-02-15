66e3d1d9667d2023ef8d97f7432001a5c561ee8b
junwon1997 <junwon20170339@gmail.com>
2023-02-15 14:15:42 +0900

    fix: ranking method 수정

--- src/main/java/com/umc/pureum/domain/use/entity/UsePhone.java
+++ src/main/java/com/umc/pureum/domain/use/entity/UsePhone.java
@@ -4,11 +4,17 @@ import com.umc.pureum.domain.user.entity.UserAccount;
 import com.umc.pureum.global.entity.BaseEntity;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
 import java.sql.Time;
 import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
+import static com.umc.pureum.domain.use.entity.UseStatus.A;
+
 @DynamicInsert
 @Entity
 @Getter
@@ -34,10 +40,12 @@ public class UsePhone extends BaseEntity {
         this.user = user;
         this.useTime = useTime;
         this.count = count;
+        this.status = A;
     }
 
     public void updateUsePhone(Time useTime, int count){
         this.useTime = useTime;
         this.count = count;
     }
+
 }
