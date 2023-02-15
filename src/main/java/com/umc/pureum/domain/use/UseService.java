56c0b406cb51a56e189f047db669386179e01d60
junwon1997 <junwon20170339@gmail.com>
2023-02-15 14:15:42 +0900

    fix: ranking method 수정

--- src/main/java/com/umc/pureum/domain/use/UseService.java
+++ src/main/java/com/umc/pureum/domain/use/UseService.java
@@ -20,7 +20,10 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
+
+import static com.umc.pureum.domain.use.entity.UseStatus.A;
 
 
 @Service
@@ -37,19 +40,26 @@ public class UseService {
             UsePhone use = useRepository.findTop1ByUserIdOrderByCreatedAtDesc(userId); // 최근에 생성된 use 테이블 가져오기
             // 입력받은 시간(String -> Time) 변환
             Time useTime = getUseTime(postUseTimeAndCountReq);
-            if (getDate(use.getCreatedAt()).equals(getDate(new Timestamp(new Date().getTime())))) {
+            if (getDate(use.getCreatedAt(),0).equals(getDate(new Timestamp(new Date().getTime()),-1))) {
                 use.updateUsePhone(useTime, postUseTimeAndCountReq.getCount());
                 return new PostUseTimeAndCountRes(use.getId());
             } else { // 최근에 생성된 테이블이 없다면...(목표 설정을 하지 않았다면...)
-                UsePhone newUse = getUsePhone(new UsePhone(use.getUser(), useTime, postUseTimeAndCountReq.getCount()));
+                UsePhone newUse = getUsePhone(use.getUser(), useTime, postUseTimeAndCountReq);
                 return new PostUseTimeAndCountRes(newUse.getId());
             }
         } catch (EmptyResultDataAccessException e){  // 처음 앱을 깔고 목표 설정도 안했다면...
             Time useTime = getUseTime(postUseTimeAndCountReq);
             Optional<UserAccount> user = userRepository.findById(userId);
-            UsePhone usePhone = getUsePhone(new UsePhone(user.get(), useTime, postUseTimeAndCountReq.getCount()));
+            UsePhone usePhone = getUsePhone(user.get(), useTime, postUseTimeAndCountReq);
             return new PostUseTimeAndCountRes(usePhone.getId());
         }
+    }
+
+    @NotNull
+    private UsePhone getUsePhone(UserAccount user, Time useTime, PostUseTimeAndCountReq postUseTimeAndCountReq) {
+        UsePhone usePhone = getUsePhone(new UsePhone(user, useTime, postUseTimeAndCountReq.getCount()));
+        usePhone.updateCreatedAt(getYesterday());
+        return usePhone;
     }
 
     @NotNull
@@ -66,14 +76,14 @@ public class UseService {
     }
 
     /* 날짜 계산 */
-    public String getDate(Timestamp created_at) {
-        Date date = new Date(created_at.getTime());
+    public String getDate(Timestamp timestamp, int num) {
+        Date date = new Date(timestamp.getTime());
         Calendar cal = Calendar.getInstance();
 
         cal.setTime(date);
-        cal.add(Calendar.DATE, 0);
+        cal.add(Calendar.DATE, num);
 
-        SimpleDateFormat format = new SimpleDateFormat("YYYY-MM-DD");
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
         return format.format(cal.getTime());
     }
@@ -107,4 +117,15 @@ public class UseService {
 
         return new ReturnGradeRes(userId , grade);
     }
+
+    public Timestamp getYesterday() {
+        Date date = new Date();
+        Calendar cal = Calendar.getInstance();
+
+        cal.setTime(date);
+        cal.add(Calendar.DATE, -1);
+
+        return new Timestamp(cal.getTime().getTime());
+    }
+
 }
